aef8b9396b1f273d9a0cccf80d634cf46dde854b
Lucas <lucasmgs21@gmail.com>
2020-03-30 20:48:19 -0300

    first commit

---
+++ word2pdf/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace word2pdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox_fileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBox_fileName
+            // 
+            this.txtBox_fileName.Location = new System.Drawing.Point(24, 34);
+            this.txtBox_fileName.Name = "txtBox_fileName";
+            this.txtBox_fileName.Size = new System.Drawing.Size(206, 20);
+            this.txtBox_fileName.TabIndex = 1;
+            this.txtBox_fileName.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome do arquivo word:";
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(89, 60);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(75, 23);
+            this.btnConverter.TabIndex = 3;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.BtnConverter_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Obs: O arquivo word tem que estar no desktop";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 123);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConverter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox_fileName);
+            this.Name = "Form1";
+            this.Text = "Word to PDF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox_fileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ word2pdf/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using SautinSoft.Document;
+
+namespace word2pdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void BtnConverter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fileName = txtBox_fileName.Text;
+                var path = @"C:\Users\Lucas Moreira\Desktop";
+                var pathFile = path + "\\" + fileName + ".docx";
+                var pdfFile = pathFile.Replace(".docx",".pdf");
+                
+                var dc = DocumentCore.Load(pathFile);
+                dc.Save(pdfFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro:", ex.Message);
+            }
+        }
+    }
+}
